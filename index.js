df503835a3c119736832f4fb7d4413144c802831
Irina Ashurova <you-ve_got@mail.ru>
2023-04-18 20:49:16 -0400

    done

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 // Код для первого задания напиши в этой функции
-function crawlTree() {}
+function crawlTree(arr) {
+  let result = ''
+  for (let i=0; i<arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+    result +=crawlTree(arr[i])
+  } else {
+      result +=arr[i]
+  }
+}
+return result
+}
 
 // Код для второго задания напиши в этой функции
-function twoSum() {}
+function twoSum(arr, target) {
+    function check(i, j) {
+      if (arr[i]+arr[j] === target) {
+       return [i, j]
+    }
+      if (j<arr.length) {
+        return check(i, j+1)
+      }
+      else {
+        return check(i+1, j+2)
+      }
+  }
+   return check(0, 1)
+  }
 
 // Всё, что ниже, нужно для тестов. Не изменяй этот код
 const functions = { crawlTree, twoSum };
